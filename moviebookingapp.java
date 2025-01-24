36567df9cd9e436ed595a78c885a4313411c4a1f
Saadahmed6633 <144558126+Saadahmed6633@users.noreply.github.com>
2025-01-24 15:10:56 +0500

    Update moviebookingapp.java

--- moviebookingapp.java
+++ moviebookingapp.java
@@ -26,7 +26,7 @@ public class moviebookingapp {
         }
     }
 
-    public static void main(String[] args) {
+    public static void version2main(String[] args) {
         moviebookingapp app = new moviebookingapp();
 
     
